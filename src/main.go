7e944c4d500205fe9957b9d865354e5a5e640508
ryqdev <ykshang4119@gmail.com>
2024-09-16 23:16:03 +0800

    feat: add webhook

--- src/main.go
+++ src/main.go
@@ -4,10 +4,12 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/ryqdev/golog"
 )
 
 func main() {
 	r := gin.Default()
+	golog.ShowDetail(true)
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Hello CICD",
@@ -16,6 +18,17 @@ func main() {
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
+		})
+	})
+	r.POST("/webhook", func(c *gin.Context) {
+		// get and print header and body
+		header := c.Request.Header
+		body := c.Request.Body
+		golog.Info("header: ", header)
+		golog.Info("body: ", body)
+		c.JSON(http.StatusOK, gin.H{
+			"header": header,
+			"body":   body,
 		})
 	})
 	r.Run()
